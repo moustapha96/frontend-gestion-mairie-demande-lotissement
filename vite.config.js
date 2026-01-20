cf14b517ccdb769330b57b9ea2f1ddc97f001863
Al hussein Khouma/Full Stack Developper <68778997+moustapha96@users.noreply.github.com>
2026-01-20 12:41:32 +0000

    addd

--- vite.config.js
+++ vite.config.js
@@ -3,49 +3,11 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import tailwind from "tailwindcss";
 
-import { VitePWA } from "vite-plugin-pwa";
-
-
-const pwaConfig = {
-    registerType: "autoUpdate",
-    includeAssets: ["logo-color.svg"],
-    workbox: {
-        globPatterns: ["**/*.{js,css,html,png,jpg,gif,svg,jpeg}"],
-        navigateFallback: "/",
-        navigateFallbackAllowlist: [/^(?!\/__).*/],
-        runtimeCaching: [{
-            urlPattern: /\.(png|jpg|gif|svg)$/,
-            handler: "StaleWhileRevalidate",
-        }, ],
-        maximumFileSizeToCacheInBytes: 10 * 1024 * 1024,
-    },
-    manifest: {
-        name: "gestion-mairie",
-        short_name: "gestion-mairie",
-        description: "Gestion de la Mairie de Kaolack",
-        start_url: "/",
-        display: "standalone",
-        background_color: "#e28743",
-        theme_color: "#eab676",
-        icons: [{
-                src: "/logo_150X150.png",
-                sizes: "192x192",
-                purpose: "any maskable",
-            },
-            {
-                src: "/logo_150X150.png",
-                sizes: "512x512",
-                purpose: "maskable any",
-            },
-        ],
-    },
-};
-
 // https://vitejs.dev/config/
 export default defineConfig({
     basename: "/",
     // plugins: [react()],
-    plugins: [react(), VitePWA(pwaConfig), tailwind()],
+    plugins: [react(), tailwind()],
     define: { "process.env": {} },
     build: {
         outDir: "../deploy-mairie/",
@@ -66,5 +28,17 @@ export default defineConfig({
     server: {
         port: 3000,
         open: true,
+        headers: {
+            "Cache-Control": "no-store, no-cache, must-revalidate, max-age=0",
+            "Pragma": "no-cache",
+            "Expires": "0",
+        },
+    },
+    preview: {
+        headers: {
+            "Cache-Control": "no-store, no-cache, must-revalidate, max-age=0",
+            "Pragma": "no-cache",
+            "Expires": "0",
+        },
     },
 });
